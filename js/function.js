712f920e1c4d21a1d4d4302cef27a3f040d2625c
Marcus Gonçalves <marquinhusgoncalves@hotmail.com>
2015-10-31 17:06:02 -0200

    Aula 3

--- js/function.js
+++ js/function.js
@@ -33,6 +33,76 @@ $(".novoCartao").submit(function(event){
 											 .replace(/\*(.+)\*/g, "<em>$1</em>");
 
 	if(conteudo) {
+
+	adicionaCartao(conteudo);
+
+	}
+
+	campoConteudo.val("");
+	//event.preventDefault();
+});
+
+function decideTipoCartao(conteudo) {
+	var quebras = conteudo.split("<br>").length;
+	var totalDeLetras = conteudo.replace(/<br>/g," ").length;
+
+	var ultimoValor = conteudo.replace(/<br>/g, " ")
+							  .split(" ")
+							  .reduce(function(ultimaPalavra, palavraAtual) {
+									if (palavraAtual > ultimaPalavra) {
+										ultimaPalavra = palavraAtual;
+									} return ultimaPalavra;
+								}, " ")
+
+					//Substituição do .forEach por .reduce		  		
+					//.forEach(function(palavra) {
+					//	if(palavra.length > ultimoValor.length) {
+					//		ultimoValor = palavra;
+					//	}
+					//});
+
+					var tamMaior = ultimoValor.length;
+
+					//no mínimo, todo cartão tem o texto pequeno
+					var tipoCartao = "cartao--textoPequeno";
+
+					if (tamMaior < 9 && quebras < 5 && totalDeLetras < 55) {
+						tipoCartao = "cartao--textoGrande";
+					} else if (tamMaior < 12 && quebras < 6 && totalDeLetras < 75) {
+						tipoCartao = "cartao--textoMedio";
+					}
+					return tipoCartao;
+}
+
+$("#busca").on("input", function () {
+	//guarda o valor digitado,removendoespaços extras.
+	var busca = $(this).val().trim();
+	if(busca.length) {
+		$(".cartao").hide().filter(function() {
+			return $(this).find(".cartao-conteudo")
+						  .text()
+						  .match(new RegExp(busca, "i"));
+		}).show();
+	} else {
+		$(".cartao").show();
+	}
+});
+
+$("#ajuda").click(function() {
+	$.getJSON("https://ceep.herokuapp.com/cartoes/instrucoes", 
+		function(res) {
+			console.log(res);
+
+			res.instrucoes.forEach(function(instrucao) {
+				adicionaCartao(instrucao.conteudo, instrucao.cor);
+			});
+		}
+	);
+});
+
+function adicionaCartao(conteudo, cor) {
+
+	contador ++;
 		var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
 									   .attr("data-ref", contador)
 									   .text("Remover")
@@ -41,16 +111,16 @@ $(".novoCartao").submit(function(event){
 		var opcoes = $("<div>").addClass("opcoesDoCartao")
 							   .append(botaoRemove);
 
+		var tipoCartao = decideTipoCartao(conteudo);
+		
 		var conteudoTag = $("<p>").addClass("cartao-conteudo")
 								  .append(conteudo);
 
-		$("<div>").attr("id", "cartao_" + contador)
+		$("<div>").attr("id","cartao_" + contador)
 				  .addClass("cartao")
+				  .addClass(tipoCartao)
 				  .append(opcoes)
 				  .append(conteudoTag)
+				  .css("background-color", cor)
 				  .prependTo(".mural");
 	}
-
-	campoConteudo.val("");
-	event.preventDefault();
-});
